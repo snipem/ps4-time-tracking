4d00b71ec878d6e7df3a2607f8a868323b18c79a
snipem <mail@matthias-kuech.de>
2015-09-27 20:16:50 +0200

    Changed log directory

--- ps4watch.sh
+++ ps4watch.sh
@@ -2,4 +2,4 @@
 json=`ps4wake -P -B -j`;
 timestamp=`echo $json | jq .timestamp`
 dateCheck=`python -c "from datetime import datetime; print (datetime.now().isoformat())"`
-echo "$dateCheck,$(echo $json | jq .running_app_name),$(echo $json | jq .running_app_titleid)" >> ps4watch.log
+echo "$dateCheck,$(echo $json | jq .running_app_name),$(echo $json | jq .running_app_titleid)" >> ~/log/ps4watch.log
