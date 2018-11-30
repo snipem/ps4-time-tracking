d5109e6de3a60a3564621ce22f1ecbda5ec04c22
Matthias Küch <mail@matthias-kuech.de>
2018-11-30 22:57:48 +0100

    Adapt shellscript to python logic

--- ps4watch.sh
+++ ps4watch.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 dir="$HOME/log"
 [ -d $dir ] || mkdir -p "$dir"
-json=`ps4wake -P -B -j` &&
-timestamp=`echo $json | jq .timestamp` &&
-dateCheck=`python -c "from time import gmtime, strftime; print (strftime('%Y-%m-%dT%H:%M+0000',gmtime()))"` &&
-echo "$dateCheck,$(echo $json | jq .running_app_name),$(echo $json | jq .running_app_titleid)" >> $dir/ps4watch.log
+./ps4watch.py >> $dir/ps4watch.log
 
