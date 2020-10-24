03c966aa1c1e8ffc6b83a8f84cbdc420705318fa
Matthias Küch <mail@matthias-kuech.de>
2020-10-24 11:53:45 +0200

    Fix for switched off PS4 ending up in exception

--- ps4watch.py
+++ ps4watch.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
+# run: ./ps4watch.py
 
 import pyps4
 import datetime
+import socket
 
 playstation = pyps4.Ps4("0.0.0.0", broadcast=True)
 
@@ -15,7 +17,10 @@ try:
     appName = playstation.get_running_app_name()
     titleId = playstation.get_running_app_titleid()
 except KeyError as err:
-    # Expected error if PS4 is not online
+    # Expected error if PS4 is in standby mode
+    pass
+except socket.timeout as err:
+    # Expected error if PS4 is switched off
     pass
 
 print("%s,%s,%s" %
